d3bbc7e8bccbf5dabcffa2bcbd3502c8563da663
Glitch (glitch-hello-node) <none>
2024-11-16 21:39:26 +0000

    🚖👰 Checkpoint ./server.js:71878491/594

--- server.js
+++ server.js
@@ -2,6 +2,30 @@
  * This is the main Node.js server script for your project
  * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
  */
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Nastavení serveru
+app.use(express.static('public'));
+
+// Server začne naslouchat na portu
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// Základní obsluha Socket.IO
+io.on('connection', (socket) => {
+  console.log('User connected');
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
 
 const path = require("path");
 
