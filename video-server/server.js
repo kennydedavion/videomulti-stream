62790c9432cd40e300dee8a32ecf523441362d78
Glitch (jinyvesmir) <none>
2024-11-18 23:05:23 +0000

    🖇🚄 Checkpoint ./video-server/server.js:71878491/1730 ./package.json:71878491/2

--- video-server/server.js
+++ video-server/server.js
@@ -1,3 +1,5 @@
+// video-server/server.js
+
 const express = require('express');
 const WebSocket = require('ws');
 const http = require('http');
@@ -5,16 +7,21 @@ const http = require('http');
 const app = express();
 const port = 4000;  // Použijeme jiný port, např. 4000
 
+// Vytvoření HTTP serveru
 const server = http.createServer(app);
+
+// Inicializace WebSocket serveru
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
   console.log('Nový klient připojen.');
 
+  // Poslouchání zpráv od klienta
   ws.on('message', (message) => {
     console.log('Přijato zprávu:', message);
 
-     wss.clients.forEach((client) => {
+    // Posíláme zprávu zpět všem připojeným klientům
+    wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(`Server obdržel zprávu: ${message}`);
       }
