56999b25951f1c707ee523bf54247e70c1024bff
Glitch (puffy-mercury-circle) <none>
2024-11-18 16:12:06 +0000

    🍠📫 Checkpoint ./package.json:71878491/8795 ./server.js:71878491/113

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const WebSocket = require('ws'); // Import WebSocket knihovny
-const PORT = process.env.PORT || 3001; // Změna na 3001
+// const PORT = process.env.PORT || 3001; // Změna na 3001
 
 const app = express();
 const server = http.createServer(app);
