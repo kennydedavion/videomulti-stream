f81023bbcc80b2c6d1d7715f6d447f9c6f3f9902
Glitch (puffy-mercury-circle) <none>
2024-11-18 16:23:39 +0000

    🐷😘 Checkpoint ./package.json:71878491/39 ./server.js:71878491/2

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ function broadcastUserCount() {
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Port nastavení
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Server běží na portu ${PORT}`);
 });
