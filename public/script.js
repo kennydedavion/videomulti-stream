b7e4d443e17dfb8a4b281fc532e467b550065538
Glitch (jinyvesmir) <none>
2024-11-24 19:43:32 +0000

    Rewound to commit 30d4e6ef3089bea6d1ae0f8b2e5c548374bc7897 + 0 edits

--- public/script.js
+++ public/script.js
@@ -9,8 +9,9 @@ document.addEventListener('DOMContentLoaded', function () {
   
   let localStream;
   let peerConnections = {};
-  const socket = new WebSocket('wss://yourserverurl.com'); // Upravte URL na váš WebSocket server
-  
+ const socket = new WebSocket('ws://puffy-mercury-circle.glitch.me');
+ // Upravte URL na vconst socket = new WebSocket('ws://puffy-mercury-circle.glitch.me');
+ 
   // Připojení na WebSocket server
   socket.onopen = () => {
     console.log('Připojeno k WebSocket serveru');
